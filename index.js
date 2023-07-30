9c49a8ae05973edf8224e2e3266120a387038db2
Fekadu zaza <fkdzaza@gmail.com>
2023-07-30 16:46:49 +0300

    Digital clock with HTML,CSS and JS

---
+++ index.js
@@ -0,0 +1,26 @@
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("meridian");
+
+function digitalClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+  h = h < 10 ? `0 ${h}` : h;
+  hourEl.innerText = h;
+  m = m < 10 ? `0 ${m}` : m;
+  minuteEl.innerText = m;
+  s = s < 10 ? `0 ${s}` : s;
+  secondEl.innerText = s;
+  ampmEl, (innerText = ampm);
+  setTimeout(() => {
+    digitalClock();
+  }, 1000);
+}
+digitalClock();
